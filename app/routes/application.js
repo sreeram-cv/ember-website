8ce7512a57a18297cc8a50254984156418df70cc
sreeram c v <sreeram.venkitaraman@freshworks.com>
2025-04-09 17:43:23 +0530

    cspell checker initial integration

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default class AplicationRoute extends Route {
+export default class ApplicationRoute extends Route {
   @service navbar;
   @service metrics;
   @service fastboot;
